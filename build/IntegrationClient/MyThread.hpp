46c7db2a05602050aa98aae8de78fc7135afe72a
Amaresh Kumar <amaresh.kumar@live.in>
2017-12-29 22:37:25 +0530

    simple working thread functionality using pthread

---
+++ build/IntegrationClient/MyThread.hpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   MyThread.hpp
+ * Author: akumar
+ *
+ * Created on 27 September, 2015, 4:58 PM
+ */
+
+#ifndef MYTHREAD_HPP
+#define	MYTHREAD_HPP
+
+#include "my_project_app.h"
+
+#define HAVE_STRUCT_TIMESPEC
+
+#include <pthread.h>
+#include <thread>
+#include <chrono>
+#include <iostream>
+//to use function sleep
+#include <stdlib.h>
+#include <cstdlib>
+
+//Mutex
+void* showUpdatedBalance (void *null);
+void thread_func_mutex ();
+
+//condition variable
+void* watch_balance (void* null);
+void* update_balance_cv (void* null);
+void thread_func_cond();
+
+#endif	/* MYTHREAD_HPP */
+
