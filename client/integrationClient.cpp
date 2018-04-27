d49df3fba18c26aa74b5312d6f6ece49a04de369
Amaresh Kumar <ashatrug@visteon.com>
2018-04-27 17:25:38 +0530

    new tree implementation | code has to be optimized

--- client/integrationClient.cpp
+++ client/integrationClient.cpp
@@ -3,16 +3,19 @@
 
 int main () {
 
-    //Disable temporerly for direct testing
+    //commented for direct testing
 	//utility::start ();
 
 	int arr [] = {15, 10, 100, 5, 13, 90, 80, 50, 10, 5, 3};
+    int len = sizeof(arr)/sizeof(*arr);
+
+	using namespace btree;
 	//create an instance of btree::tree;
-	//tree* tree = new tree();
-	//tree->create_btree (arr);
+	btree::tree* tree = new btree::tree();
+	tree->create_btree (arr, len);
 
 	//now display tree
-//	tree->display_btree_i (tree->get_root_node());
+	tree->display_btree_i (tree->get_root_btnode());
 
 	return 0;
 }
--- inc/btree.hpp
+++ inc/btree.hpp
@@ -2,14 +2,16 @@
 #define BTREE_H_
 
 //btnode class
+
+namespace btree {
 class btnode {
 public:
 	btnode(int data): m_info(data), m_left(nullptr), m_right (nullptr) {}
 
-	btnode* get_left_btnode ()  { return this->m_left; }
-	btnode* get_right_btnode () { return this->m_right; }
-	int get_data () { return this->m_info;}
-private:
+	btnode* get_left_btnode ()  { return m_left; }
+	btnode* get_right_btnode () { return m_right; }
+	int get_data () { return m_info;}
+public:
 	int m_info;
 	btnode* m_left;
 	btnode* m_right;
@@ -19,18 +21,21 @@ private:
 class tree {
 public:
 	//construct a complete binary tree
-	tree (): m_root_btnode (nullptr) {}
+	tree ();
+	~tree();
 
 	//create a tree: complete tree
-	void create_btree (int arr[]);
+	void create_btree (int arr[], int len);
+
 	//in-order traversal
 	void display_btree_i (btnode *root);
 	void add_a_btnode (btnode* btnode, int data);
-	//inline
+
 	inline btnode* get_root_btnode () { return m_root_btnode; }
-	~tree() { m_root_btnode = nullptr;}
+
 private:
 	btnode* m_root_btnode;
 };
 
+}
 #endif
