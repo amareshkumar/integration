2f8e474af9d2c2be502aa32d770bf33fd08c9474
amaresh kumar <amaresh.kumar@live.in>
2018-04-28 12:29:34 +0530

    modified binary tree implem

--- source/btree.cpp
+++ source/btree.cpp
@@ -2,62 +2,74 @@
 #include <iostream>
 
 using namespace std;
-namespace btree{
+namespace btree {
 
 btnode* tree::m_root_btnode = nullptr;
-	
+btnode* tree::m_root = nullptr;
+
 tree::tree () {}
 
-tree::~tree() {
-	m_root_btnode = nullptr;
-}
+//create a tree: complete tree
+tree::tree(int arr[], int len) {
+	if (len > 0) {
+		m_root_btnode = m_root = new btnode(arr[0]);
+		std::cout << "a root btnode is added with data: " << arr[0] << endl;
+	}
+	else {
+		std::cout << "please pass valid len of array!" << std::endl;
+		return;
+	}
 
-//creates a complete tree
-void tree::create_btree (btnode* root, int arr[], int len) {
-
-	for (int i = 0; i < len; i++) {
-		add_a_btnode (root, arr[i]);
+	for (int i = 1; i < len; i++) {
+		add_a_btnode(m_root_btnode, arr[i]);
 	}
 	std::cout << "Tree creation successful!!" << std::endl;
 }
 
-void tree::add_a_btnode (btnode* root, int data) {
+tree::~tree() {
+	//m_root_btnode = nullptr;
+}
+
+void tree::add_a_btnode (btnode* &root, int data) {
 	///check if tree is empty
 	btnode *current = root;
 	if (!current) {
-		btnode* newbtnode = new btnode(data);
-		root = newbtnode;
+		m_root_btnode = new btnode(data);
 		return;
 	}
 
-	if (current->get_left_btnode() != nullptr) {
+	if (current->m_left != nullptr) {
 		//can we add it as right child of current btnode?
-		if (current->get_right_btnode() != nullptr) {
-			//recurse
-			add_a_btnode (current->get_left_btnode(), current->get_data());
+		if (current->m_right != nullptr) {
+			//recurse, until you find leaf node to add keeping the tree complete
+			add_a_btnode (current->m_left->m_left, data);
 		} else {
-			btnode* newbtnode = new btnode(data);
-			current->m_right = newbtnode;
+			current->m_right = new btnode(data);
 		}
 	} else {
-		btnode* newbtnode = new btnode(data);
-//		current->get_left_btnode() = newbtnode;
-		current->m_left = newbtnode;
+		current->m_left = new btnode(data);
 	}
-	std::cout << "a root btnode is added with data: " << data << endl;
+	std::cout << "a btnode is added with data: " << data << endl;
 }
 
 //inorder traversal
-void tree::display_btree_i (btnode* root) {
+void tree::display_btree_i(btnode* &root) {
 	btnode* current = root;
-	if (current == nullptr){
-		std::cout << "No elem in tree!" << std::endl;
+	if (root == nullptr) {
 		return;
 	}
 
-	display_btree_i (current->get_left_btnode());
-	std::cout << current->get_data() << " ";
-	display_btree_i (current->get_right_btnode());
+	display_btree_i(current->m_left);
+	std::cout << current->m_info << " ";
+	display_btree_i (current->m_right);
 }
 
+void tree::display_btree_in() {
+	if (m_root == nullptr) {
+		std::cout << "No elem in tree!" << std::endl;
+		return;
+	}
+	display_btree_i(m_root);
 }
+
+} //namespace btree
