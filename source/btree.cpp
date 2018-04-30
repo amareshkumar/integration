57d3ce2805e6e3cc5245ead0d1cf1ae197344601
Amaresh Kumar <ashatrug@visteon.com>
2018-04-30 15:47:09 +0530

    [fix]: binary display issue; prime_sum issue and added height functionality

--- source/btree.cpp
+++ source/btree.cpp
@@ -42,14 +42,14 @@ void tree::add_a_btnode (btnode* &root, int data) {
 		//can we add it as right child of current btnode?
 		if (current->m_right != nullptr) {
 			//recurse, until you find leaf node to add keeping the tree complete
-			add_a_btnode (current->m_left->m_left, data);
+			add_a_btnode (current->m_left, data);
 		} else {
 			current->m_right = new btnode(data);
 		}
 	} else {
 		current->m_left = new btnode(data);
 	}
-	std::cout << "a btnode is added with data: " << data << endl;
+	//std::cout << "a btnode is added with data: " << data << endl; //todo: why this prints more times than actually it should
 }
 
 //inorder traversal
@@ -64,12 +64,33 @@ void tree::display_btree_i(btnode* &root) {
 	display_btree_i (current->m_right);
 }
 
+//to get difference of left and right subtree
+int tree::tree_hight_diff (btnode* left, btnode* right) {
+	return (height (left) - height(right));
+}
+
+int tree::height (btnode* root) {
+
+	if (!root) {
+		return 0;
+	}
+
+	return (max(height(root->m_left), height (root->m_right)) + 1);
+}
+
 void tree::display_btree_in() {
 	if (m_root == nullptr) {
 		std::cout << "No elem in tree!" << std::endl;
 		return;
 	}
 	display_btree_i(m_root);
+}
+
+int tree::height() {
+	if (m_root == nullptr) {
+		return 0;
+	}
+	return height(m_root);
 }
 
 } //namespace btree
