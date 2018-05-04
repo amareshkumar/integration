11947fa37116539f4d351a72ab58ccfa85a4be8c
Amaresh Kumar <ashatrug@visteon.com>
2018-05-04 22:17:11 +0530

    fixes #15

---
+++ source/kumar_hash.cpp
@@ -0,0 +1,17 @@
+#include "kumar_hash.hpp"
+
+namespace kumar{
+
+int hash_table::insert (int item) {
+	//todo
+	return 0;
+}
+void hash_table::remove (int key) {
+	//todo
+}
+bool hash_table::search () {
+	//todo
+	return true;
+}
+
+}
